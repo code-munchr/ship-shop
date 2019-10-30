a4c594e38549757c0d79992f48151b8d7536d8b5
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-10-30 18:09:56 +0500

    before adding  devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,7 @@ class ItemsController < ApplicationController
   # POST /items
   # POST /items.json
   def create
+    binding.pry
     @item = Item.new(item_params)
 
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :shopping_experiences
   resources :items
   resources :categories
+  # post '/users/add_to_cart_with_id/:id', to: 'user#add_to_cart_with_id', as: 'patient'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
